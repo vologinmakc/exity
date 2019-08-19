4b5ab702fb750c99551151a61968cdb32fd9d0c6
vologinmakc <vologinmakc@gmail.com>
2019-08-19 22:11:37 +0700

    create home page test

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Бычара
                 </div>
 
                 <div class="links">
